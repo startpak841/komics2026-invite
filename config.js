99dd944034df3fdc8b5fdd12004edfa434f47503
startpak841 <startpak841@gmail.com>
2026-05-29 16:11:29 +0900

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // ============================================
 
 const SUPABASE_URL = 'https://olztpirknykqdvqjlewb.supabase.co';
-const SUPABASE_ANON_KEY = 'PASTE_YOUR_ANON_KEY_HERE';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnZGd6Z2hoZHZoY3d1bWt2dXllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMzg2MjUsImV4cCI6MjA5NTYxNDYyNX0.on5K0A68OD8i9FlhaBoBcvSspmXL8bok5-VL-03n-EY';
 
 // 관리자 비밀 키 (URL에 ?key=xxx 와 일치해야 admin 접근 가능)
 const ADMIN_KEY = 'komics2026';
